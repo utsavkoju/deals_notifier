4b215976a89034b61c560aa390ff0f2433a31fad
utsavkoju <utsavkoju@hotmail.com>
2018-11-25 03:32:48 -0330

    minor update

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -16,7 +16,7 @@ class Product extends Model
     	$query->leftjoin('brands','brands.id','=','products.brand_id');
     	$query->where('products.deleted_at',null);
     	$result = $query->paginate(20); //object[array] $query->first() //single value as array
-    	return $result;
+    	return $result; // this will return result
     }
 
     public function get_details($id) {
